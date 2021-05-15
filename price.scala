d46dc32e74707a2bb4554972232fd7cb19a83498
NIPUNMADHUSANKA <nipunmadhusanka201085@gmail.com>
2021-05-15 05:51:44 +0000

    change shiping cost

--- price.scala
+++ price.scala
@@ -2,7 +2,7 @@ object price extends App{
   
   def discount(y:Double) = (y * 60) / 100;
 
-  def ship(p:Int):Double =  if (p-50 > 0) (50*3+(p-50)*0.75) else (50*3);
+  def ship(p:Int):Double =  if (p-50 > 0) (3+(p-50)*0.75) else (3);
 
   def cal(x:Int):Double =  discount(x * 24.95) + ship(x) ;
 
